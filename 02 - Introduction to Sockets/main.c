a81dd37aa0f820c7ec8c8e95e0dab2595927ce8b
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2024-04-27 15:44:47 +0200

    Add loop to server

--- 02 - Introduction to Sockets/main.c
+++ 02 - Introduction to Sockets/main.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -12,7 +13,14 @@
 
 #define SOCKET int
 
+volatile sig_atomic_t execute = 1;
+void trap(int signal) {
+  execute = 0;
+}
+
 int main() {
+  signal(SIGINT, trap);
+
   printf("Configurtion local address...\n");
 
   struct addrinfo hints;
@@ -22,7 +30,7 @@ int main() {
   hints.ai_flags = AI_PASSIVE;
 
   struct addrinfo *bind_address;
-  if (getaddrinfo(NULL, "8080", &hints, &bind_address) != 0) {
+  if (getaddrinfo(NULL, "8082", &hints, &bind_address) != 0) {
     fprintf(stderr, "getaddrsinfo failed");
     return 1;
   }
@@ -52,44 +60,46 @@ int main() {
     return 1;
   }
 
-  printf("Waiting for connection...\n");
-  struct sockaddr_storage client_address;
-  socklen_t client_len = sizeof(client_address);
-  SOCKET socket_client =
-      accept(socket_server, (struct sockaddr *)&client_address, &client_len);
-  if (socket_client < 0) {
-    fprintf(stderr, "Failed to accept connection (%d)", errno);
-    return 1;
+  while (execute) {
+    printf("Waiting for connection...\n");
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+    SOCKET socket_client =
+        accept(socket_server, (struct sockaddr *)&client_address, &client_len);
+    if (socket_client < 0) {
+      fprintf(stderr, "Failed to accept connection (%d)", errno);
+      continue;
+    }
+
+    char address_buffer[128];
+    getnameinfo((struct sockaddr *)&client_address, client_len, address_buffer,
+                sizeof(address_buffer), NULL, 0, NI_NUMERICHOST);
+    printf("Client '%s' is connected...\n", address_buffer);
+
+    printf("Reading request...\n");
+    char request[1024];
+    int bytes_recieved = recv(socket_client, request, sizeof(request), 0);
+    // TODO check if bytes_recieved >= 0 in PROD
+    printf("%.*s", bytes_recieved, request);
+
+    printf("Sending response...\n");
+    const char *response = "HTTP/1.1 200 OK\r\n"
+                           "Connection: close\r\n"
+                           "Content0Type:text/plain\r\n\r\n"
+                           "Local time is: ";
+    int bytes_sent = send(socket_client, response, (int)strlen(response), 0);
+    // TODO check if bytes_sent >= 0 in PROD
+    printf("Sent %d of %d bytes\n", bytes_sent, (int)strlen(response));
+
+    time_t timer;
+    time(&timer);
+    char *time_msg = ctime(&timer);
+    bytes_sent = send(socket_client, time_msg, (int)strlen(time_msg), 0);
+    printf("Sent %d of %d bytes\n", bytes_sent, (int)strlen(time_msg));
+
+    printf("Closing conection... \n");
+    close(socket_client);
   }
-
-  char address_buffer[128];
-  getnameinfo((struct sockaddr *)&client_address, client_len, address_buffer,
-              sizeof(address_buffer), NULL, 0, NI_NUMERICHOST);
-  printf("Client '%s' is connected...\n", address_buffer);
-
-  printf("Reading request...\n");
-  char request[1024];
-  int bytes_recieved = recv(socket_client, request, sizeof(request), 0);
-  // TODO check if bytes_recieved >= 0 in PROD
-  printf("%.*s", bytes_recieved, request);
-
-  printf("Sending response...\n");
-  const char *response = "HTTP/1.1 200 OK\r\n"
-                         "Connection: close\r\n"
-                         "Content0Type:text/plain\r\n\r\n"
-                         "Local time is: ";
-  int bytes_sent = send(socket_client, response, (int)strlen(response), 0);
-  // TODO check if bytes_sent >= 0 in PROD
-  printf("Sent %d of %d bytes\n", bytes_sent, (int)strlen(response));
-
-  time_t timer;
-  time(&timer);
-  char *time_msg = ctime(&timer);
-  bytes_sent = send(socket_client, time_msg, (int)strlen(time_msg), 0);
-  printf("Sent %d of %d bytes\n", bytes_sent, (int)strlen(time_msg));
-
-  printf("Closing conection... \n");
-  close(socket_client);
 
   printf("Closing server socket...\n");
   close(socket_server);
